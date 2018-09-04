6fae649e1d7d02de719ec14738ec07e9c1ad820f
Karl J. Overå <k@rl.run>
2018-09-04 22:35:32 +0200

    Add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,18 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "karl.run",
+        short_name: "karl.run",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "##36a398",
+        display: "minimal-ui",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
