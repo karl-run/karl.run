353b8b93ce6c2ba772c589e146009380150d60c0
Karl J. Overå <k@rl.run>
2022-03-13 19:58:31 +0100

    🤫

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ function MyDocument(): JSX.Element {
       <Head>
         <link rel="stylesheet" href="https://unpkg.com/prism-themes@1.6.0/themes/prism-synthwave84.css" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=optional" rel="stylesheet" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4367385676944782"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
