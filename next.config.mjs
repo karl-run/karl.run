6302bbcd3980ae0c149657805764371927fcbf64
Karl J. Overå <k@rl.run>
2023-04-04 14:33:34 +0200

    set up loading of mdx files

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,44 @@
-import nextMdx from "@next/mdx";
-import remarkFrontmatter from "remark-frontmatter";
-import rehypeHighlight from "rehype-highlight";
+import nextMdx from '@next/mdx';
+import remarkFrontmatter from 'remark-frontmatter';
+import rehypeHighlight from 'rehype-highlight';
 
 const withMDX = nextMdx({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [remarkFrontmatter],
-    rehypePlugins: [rehypeHighlight],
-  }
-})
+    rehypePlugins: [[rehypeHighlight, { ignoreMissing: true }]],
+  },
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async redirects() {
+    return [
+      {
+        source: '/2018/09/05/spring-boot-schema-first-graphql',
+        destination: '/blog/spring-boot-schema-first-graphql',
+        permanent: true,
+      },
+      {
+        source: '/2018/05/07/kotlin-spring-boot-react',
+        destination: '/blog/kotlin-spring-boot-react',
+        permanent: true,
+      },
+      {
+        source: '/2017/07/12/publishing-npm-modules',
+        destination: '/blog/publishing-npm-modules',
+        permanent: true,
+      },
+      {
+        source: '/2017/06/30/getting-up-and-running-with-testing-react',
+        destination: '/blog/getting-up-and-running-with-testing-react',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
---
+++ src/app/blog/kotlin-spring-boot-react/page.tsx
@@ -0,0 +1,7 @@
+import Post from './index.mdx';
+
+function Page(): JSX.Element {
+  return <Post />;
+}
+
+export default Page;
