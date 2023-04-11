c62c5c4aa5f13e79260571d03befc1395e927b1b
Karl J. Overå <k@rl.run>
2023-04-11 23:25:21 +0200

    also sort on project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import { getProjectsMetadata } from '@/utils/projects';
 import LastUpdatedCommit from '@/components/LastUpdatedCommit';
 import { Card } from '@/components/EntryCard';
+import * as R from 'remeda';
 
 function Page(): JSX.Element {
   const projects = getProjectsMetadata();
@@ -8,20 +9,24 @@ function Page(): JSX.Element {
     <main className="mx-4">
       <h2 className="text-shadow-dark mb-4 text-3xl">All projects</h2>
       <div className="flex flex-row flex-wrap gap-4">
-        {projects.map(([project, metadata]) => {
-          return (
-            <Card
-              key={metadata.date}
-              href={`/projects/${project}`}
-              title={metadata.title}
-              subtitle={metadata.excerpt}
-              date={metadata.date}
-              // @ts-expect-error Async RSC
-              extraDate={<LastUpdatedCommit name={project} />}
-              projectValues={metadata.projectValues}
-            />
-          );
-        })}
+        {R.pipe(
+          projects,
+          R.sortBy([(it) => it[1].date, 'desc']),
+          R.map(([project, metadata]) => {
+            return (
+              <Card
+                key={metadata.date}
+                href={`/projects/${project}`}
+                title={metadata.title}
+                subtitle={metadata.excerpt}
+                date={metadata.date}
+                // @ts-expect-error Async RSC
+                extraDate={<LastUpdatedCommit name={project} />}
+                projectValues={metadata.projectValues}
+              />
+            );
+          }),
+        )}
       </div>
     </main>
   );
