9c4f80c7b01cfca708d6e81ca9a5e8e67390fef9
Karl J. Overå <k@rl.run>
2023-04-11 23:22:47 +0200

    basic all projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,30 @@
+import { getProjectsMetadata } from '@/utils/projects';
+import LastUpdatedCommit from '@/components/LastUpdatedCommit';
+import { Card } from '@/components/EntryCard';
+
 function Page(): JSX.Element {
-  return <div>projects</div>;
+  const projects = getProjectsMetadata();
+  return (
+    <main className="mx-4">
+      <h2 className="text-shadow-dark mb-4 text-3xl">All projects</h2>
+      <div className="flex flex-row flex-wrap gap-4">
+        {projects.map(([project, metadata]) => {
+          return (
+            <Card
+              key={metadata.date}
+              href={`/projects/${project}`}
+              title={metadata.title}
+              subtitle={metadata.excerpt}
+              date={metadata.date}
+              // @ts-expect-error Async RSC
+              extraDate={<LastUpdatedCommit name={project} />}
+              projectValues={metadata.projectValues}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
 }
 
 export default Page;
