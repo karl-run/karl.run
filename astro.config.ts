6a01b295e6d66b3b5664b583cc2bd15d1bfcd182
Karl J. Overå <k@rl.run>
2026-05-02 19:05:56 +0200

    chore: cloudflare it up

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  output: "static",
   integrations: [mdx()],
   markdown: {
     shikiConfig: { theme: 'synthwave-84' },
